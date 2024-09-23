e201c4ba98fdd213679328801ec6421b9f6eb9d1
Hayden <122996619+Hayden1610@users.noreply.github.com>
2024-09-23 17:33:50 -0400

    Step 3 : Initial Source Code

--- ProjectVA2/Source.cpp
+++ ProjectVA2/Source.cpp
@@ -1,7 +1,40 @@
 #include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<sstream>
 
-int main() 
+using namespace std;
+
+struct STUDENT_DATA //struct for student data
 {
-	return 1;
+    string Firstname;
+    string Lastname;
+};
 
+int main()
+{
+    ifstream myfile("StudentData.txt"); //opening file for student data
+    if (!myfile.is_open()) {
+        cout << "Error opening file" << endl;
+        return 1;
+    }
+
+    vector<STUDENT_DATA> student; //creating vector space
+
+    string line; //parsing the data
+    while (getline(myfile, line)) {
+        stringstream ss(line);
+        string firstName, lastName;
+        if (getline(ss, firstName, ',') && getline(ss, lastName)) {
+            STUDENT_DATA tempstudentdata;
+            tempstudentdata.Firstname = firstName;
+            tempstudentdata.Lastname = lastName;
+            student.push_back(tempstudentdata); //pushing the data into the vector space
+        }
+    }
+    myfile.close();
+
+    return 0;
 }
