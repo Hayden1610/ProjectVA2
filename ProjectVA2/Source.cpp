99495524a49f92cc82fb3f1aa14910c9a8c896fa
Hayden <122996619+Hayden1610@users.noreply.github.com>
2024-09-23 22:24:12 -0400

    Step 4: Adding in some _DEBUG functionality

--- ProjectVA2/Source.cpp
+++ ProjectVA2/Source.cpp
@@ -1,49 +1,68 @@
-#include<stdio.h>
-#include<iostream>
-#include<fstream>
-#include<vector>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-struct STUDENT_DATA //struct for student data
+#define PRE_RELEASE
+
+struct STUDENT_DATA // struct for student data
 {
     string Firstname;
     string Lastname;
+    string Email; 
 };
 
 int main()
 {
-    ifstream myfile("StudentData.txt"); //opening file for student data
-    if (!myfile.is_open()) {
-        cout << "Error opening file" << endl;
+    vector<STUDENT_DATA> student; // creating vector space
+
+#ifdef PRE_RELEASE
+    cout << "Running in Pre-release mode" << endl;
+    ifstream studentinfo("StudentData_Emails.txt"); 
+#else
+    cout << "Running in Debug mode" << endl;
+    ifstream studentinfo("StudentData.txt"); 
+#endif
+
+    if (!studentinfo.is_open())
+    {
+        cout << "Error Opening File" << endl;
         return 1;
     }
 
-    vector<STUDENT_DATA> student; //creating vector space
-
-    string line; //parsing the data
-    while (getline(myfile, line)) {
-        stringstream ss(line);
-        string firstName, lastName;
-        if (getline(ss, firstName, ',') && getline(ss, lastName)) {
-            STUDENT_DATA tempstudentdata;
-            tempstudentdata.Firstname = firstName;
-            tempstudentdata.Lastname = lastName;
-            student.push_back(tempstudentdata); //pushing the data into the vector space
-        }
+    string stline;
+    while (getline(studentinfo, stline))
+    {
+        stringstream ss(stline);
+        string firstName, lastName, email;
+        getline(ss, firstName, ',');
+        getline(ss, lastName, ','); 
+#ifdef PRE_RELEASE
+        getline(ss, email); 
+#endif
+        STUDENT_DATA tempstudentdata;
+        tempstudentdata.Firstname = firstName;
+        tempstudentdata.Lastname = lastName;
+#ifdef PRE_RELEASE
+        tempstudentdata.Email = email; 
+#endif
+        student.push_back(tempstudentdata);
     }
-    myfile.close();
+    studentinfo.close();
 
 #ifdef _DEBUG
-    cout << "Student Data in Debug mode" << endl;
     for (int i = 0; i < student.size(); i++)
     {
-        cout << "Student Name" << " : " << student[i].Firstname << "  " << student[i].Firstname << endl;
+        cout << "Student Name: " << student[i].Firstname << " " << student[i].Lastname;
+#ifdef PRE_RELEASE
+        cout << ", Email: " << student[i].Email;
+#endif
+        cout << endl;
     }
 #endif 
-
 
     return 0;
 }
